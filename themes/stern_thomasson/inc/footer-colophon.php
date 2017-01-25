238c9720dc4decf3805ecb50b4ad2d025df686bb
Mox Biggs <moxdev1@gmail.com>
2017-01-25 11:27:43 -0500

    footer flex, site pad

--- themes/stern_thomasson/inc/footer-colophon.php
+++ themes/stern_thomasson/inc/footer-colophon.php
@@ -17,7 +17,7 @@ function stern_thomasson_footer_colophon() { ?>
             $fax = get_field( 'fax', 'options' );
 
             if( $name ):
-                echo $name . "<br>";
+                ?><span class="bold-name"><?php echo $name . "<br>"; ?></span><?php
             endif;
             if ( $address):
                 echo $address . "<br>";
@@ -26,21 +26,23 @@ function stern_thomasson_footer_colophon() { ?>
                 echo $address_2 . "<br>";
             endif;
             if ( $phone):
-                echo "Phone: " . $phone . "<br>";
+                echo "Phone: "?><a href="tel:<?php echo $phone ?>"><?php echo $phone . "<br>"; ?></a><?php
             endif;
             if ( $fax):
                 echo "Fax: " . $fax . "<br>";
             endif;
             ?>
         </div>
-        <div class="associations">
-            <figure>
-                <img class="naca-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/naca-web-logo.png" alt="national association of consumer advocates logo">
-                <img class="avvo-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/avvo-logo.png" alt="avvo logo">
-            </figure>
-        </div>
-        <div class="disclaimer">
-            <p>The information on this page and website do not constitute legal advice. A thorough evaluation of your facts and circumstances is necessary before any lawyer can provide you with a competent legal opinion.</p>
+        <div class="footer-right">
+            <div class="associations">
+                <figure>
+                    <img class="naca-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/naca-web-logo.png" alt="national association of consumer advocates logo">
+                    <img class="avvo-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/avvo-logo.png" alt="avvo logo">
+                </figure>
+            </div>
+            <div class="disclaimer">
+                <p>The information on this page and website do not constitute legal advice. A thorough evaluation of your facts and circumstances is necessary before any lawyer can provide you with a competent legal opinion.</p>
+            </div>
         </div>
     </div>
 
