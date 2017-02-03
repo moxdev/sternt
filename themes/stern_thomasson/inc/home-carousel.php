46589aafca1aed551624041f3dfe7f5f9b7a25b3
Mox Biggs <moxdev1@gmail.com>
2017-02-03 14:57:02 -0500

    blow it up

--- themes/stern_thomasson/inc/home-carousel.php
+++ themes/stern_thomasson/inc/home-carousel.php
@@ -12,19 +12,13 @@ function stern_thomasson_home_carousel() {
         $imgs = get_field('carousel_images');
         if( $imgs ): ?>
             <div class="home-carousel">
-                <?php $title = get_field('carousel_title');
-                $i = 0;
-                if($title) { ?>
-                    <h1 class="wrapper">
-                    <?php if($title): ?> <span class="highlight title"><?php echo $title; ?></span><?php endif; ?>
-                    </h1>
-                <?php } ?>
                 <ul>
                     <?php foreach( $imgs as $img ): ?>
                         <li style="background-image:url(<?php echo $img['sizes']['home-carousel']; ?>)">
                             <div class="screen-reader-text">
                                 <img src="<?php echo $img['sizes']['home-carousel']; ?>" alt="<?php echo $img['alt']; ?>">
                             </div>
+                            <span><h1><?php echo $img['caption']; ?></h1></span>
                         </li>
                     <?php endforeach; ?>
                 </ul>
